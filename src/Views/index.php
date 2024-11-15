5758d85a569384ae510d4f352fb39a7fccd97cf3
Thiruveni <140165640+thiruveni2002@users.noreply.github.com>
2024-11-15 09:38:42 +0530

    merged
    updta

--- src/Views/index.php
+++
@@ -1,7 +0,0 @@
-<h1>Welcome to Simple PHP MVC Starter!</h1>
-
-<ul>
-    <?php foreach ($tutors as $tutor) : ?>
-        <li><?= $tutor->name ?> (<?= $tutor->tutorID ?>)</li>
-    <?php endforeach; ?>
-</ul>
